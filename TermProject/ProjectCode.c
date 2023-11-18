23a5837f3740f794e118bcc70ba57dfd19639c92
sriharish252 <srihari252000@gmail.com>
2023-11-18 07:55:08 +0530

    Added PIR sensor logic

--- TermProject/ProjectCode.c
+++ TermProject/ProjectCode.c
@@ -19,10 +19,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <sys/utsname.h>
+#include <pthread.h>
 
 // GPIO paths for GPIO port access
 #define PIRdir "/sys/class/gpio/gpio66/direction"
 #define PIRval "/sys/class/gpio/gpio66/value"
+#define PIRLEDdir "/sys/class/gpio/gpio67/direction"
+#define PIRLEDval "/sys/class/gpio/gpio67/value"
 
 // ON and OFF values for controlling LED light states
 #define ON 1
@@ -36,11 +40,40 @@
 
 // Function declarations
 int16_t initialize_gpios();
-void readPIRSensor(int8_t* value);
+static void writeGPIO(int8_t* light, int16_t value);
+static void readPIRSensor(int8_t* value);
+static void pirSensor();
+static void checkRFIDAuthorized();
+static void triggerAlarm(int8_t alert[20]);
+
+static void *startRoutine_pirSensor();
+static void *startRoutine_checkRFIDAuthorized();
+
+
+// Global Variables
+int16_t pirAlertTimer = 0;
+int16_t checkRFIDUser = 0;
 
 
 int main(void)
 {
+    struct utsname sysInfo;
+
+    // Use uname to retrieve system information
+    if (uname(&sysInfo) == ERROR_CODE) {
+        (void)perror("uname");
+        return ERROR_CODE; // Exit with an error code
+    }
+
+    // Print system information and project group
+    (void)printf("\nSystem name: %s\n", sysInfo.sysname);
+    (void)printf("Node name: %s\n", sysInfo.nodename);
+    (void)printf("Machine: %s\n", sysInfo.machine);
+    (void)printf("Project Group Number: 6\n");
+    (void)printf("Student Names: Poorvi Lakkadi | Prabath Reddy Sagili Venkata | Pranitha  Kakumanu | "
+           "Sai Hruthik Karumanchi | Sai Sujith Reddy Ravula | Sri Harish Jayaram\n\n");
+
+
     // Initializing GPIO ports
     if (initialize_gpios() == ERROR_CODE){
         (void)printf("Error with GPIO initialization \n");
@@ -48,12 +81,16 @@ int main(void)
     }
     (void)printf("GPIO initialization successful!\n");  // Print statement confirming GPIO port access for debugging
 
-    int8_t value[10];
-    while (1) {
-        readPIRSensor(value);
-        printf("PIR Sensor Value: %s\n", value); // Assuming the value is a character
-        usleep(500000); // Sleep for .5 second
-    }
+
+    pthread_t t_pirSensor;
+    pthread_t t_checkRFID;
+
+    // Create Threads for Traffic Signals and their corresponding WaitButtons
+    (void)pthread_create(&t_pirSensor, NULL, startRoutine_pirSensor, NULL);
+    (void)pthread_create(&t_checkRFID, NULL, startRoutine_checkRFIDAuthorized, NULL);
+
+    pthread_exit(NULL); // Waits for the child threads to exit
+    
 
 }
 
@@ -61,7 +98,7 @@ int main(void)
 int16_t initialize_gpios(){
     int16_t f=0;
 
-    // Open the RED1 LED GPIO path in ReadWrite mode
+    // Open the PIR Sensor GPIO path in ReadWrite mode
     f=open(PIRdir, O_RDWR);
     if (f <= ERROR_CODE){     // If opening file fails, we get -1 as the return value, leading to us throwing an error upstream
         (void)perror("Error opening Red Direction");
@@ -70,10 +107,27 @@ int16_t initialize_gpios(){
     (void)write(f,"in",3); // Set the port as an output port, since it's for controlling an LED device
     (void)close((int16_t)f);
 
+    // Open the PIR alert LED GPIO path in ReadWrite mode
+    f=open(PIRLEDdir, O_RDWR);
+    if (f <= ERROR_CODE){     // If opening file fails, we get -1 as the return value, leading to us throwing an error upstream
+        (void)perror("Error opening Red Direction");
+        return ERROR_CODE;
+    }
+    (void)write(f,"out",3); // Set the port as an output port, since it's for controlling an LED device
+    (void)close((int16_t)f);
+
     return 0;
 }
 
-void readPIRSensor(int8_t* value) {
+// For writing an output into the GPIO port
+static void writeGPIO(int8_t* light, int16_t value) {
+    int16_t f=0;
+    f=open(light, O_WRONLY); // Open LED value path in Write Only mode
+    value == ON ? (void)write(f,"1",1) : (void)write(f,"0",1);
+    (void)close(f);
+}
+
+static void readPIRSensor(int8_t* value) {
     int16_t f = 0;
     f = open(PIRval, O_RDONLY); // Open PIR sensor value path in Read Only mode
     if (f == -1) {
@@ -86,4 +140,52 @@ void readPIRSensor(int8_t* value) {
     }
 
     (void)close(f);
+}
+
+static void pirSensor() {
+    int8_t value[10];
+    while (1) {
+        readPIRSensor(value);
+        
+        printf("PIR Sensor Value: %s\n", value); // Assuming the value is a character
+
+        if(value[0] == '1') {
+            printf("Motion Detected!\n");
+            printf("If you are an authorized user present RFID tag now!\n"); 
+            printf("Else an Intruder alert will be triggered!\n\n");
+            checkRFIDUser = 1;
+        }
+
+        sleep(1); // Sleep for 1 second
+    }
+}
+
+static void checkRFIDAuthorized() {
+    while(1) {
+        if(checkRFIDUser == 1) {
+            //code to check RFID
+            // if valid
+            checkRFIDUser = 0;
+            // else
+            triggerAlarm("INTRUDER");
+        }
+        sleep(1);
+    }
+}
+
+static void triggerAlarm(int8_t alert[20]) {
+    printf("\n%s alert triggered\n", alert);
+    writeGPIO(PIRLEDval, 1);
+    sleep(10);
+
+    writeGPIO(PIRLEDval, 0);
+}
+
+
+static void *startRoutine_pirSensor() {
+    pirSensor();
+}
+
+static void *startRoutine_checkRFIDAuthorized() {
+    checkRFIDAuthorized();
 }
